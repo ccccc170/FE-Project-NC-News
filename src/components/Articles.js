ab93ba3781b98a70fab214c9e054223658a0d519
ccccc170 <ccccc170@protonmail.com>
2022-08-24 12:25:39 +0100

    this commit includes completed t5 with a drop down menu to filter articles displayed by tpoic

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,39 +1,82 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { getArticles } from "../utils/api";
+import { useState, useEffect } from "react";
+import { getArticles, getTopics } from "../utils/api";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const navigate = useNavigate();
+
+  const { topic } = useParams();
 
   useEffect(() => {
-    getArticles().then((fetchedArticles) => {
-      setArticles(fetchedArticles);
+    getTopics().then((fetchedTopics) => {
+      setTopics(fetchedTopics);
     });
   }, []);
 
+  useEffect(() => {
+    let params;
+    if (topic) {
+      params = { topic: topic };
+    }
+    getArticles(params).then((fetchedArticles) => {
+      setArticles(fetchedArticles);
+    });
+  }, [topic]);
+
+  const navigateTopic = (topicName) => {
+    navigate(`/${topicName}`);
+  };
+
   return (
-    <ul className="main_articles_list">
-      {articles.map((article) => {
-        return (
-          <li key={article.article_id}>
-            <h3>{article.title}</h3>
-            <h4>topic: {article.topic}</h4>
-            <h5>written by: {article.author}</h5>
-            <h5>posted: {article.created_at}</h5>
-            <p>{article.body}</p>
-            <div className="votes_and_comments">
-              <h6 className="votes_and_comments_child1">
-                votes: {article.votes}
-              </h6>{" "}
-              <h6 className="votes_and_comments_child2">
-                comments: {article.comment_count}
-              </h6>
-            </div>
-          </li>
-        );
-      })}
-      ;
-    </ul>
+    <section>
+      <label htmlFor="topic-selector">Filter by topic:</label>
+      <select name="topic-selector" id="topic-list">
+        <option
+          value="All topics"
+          onClick={() => {
+            navigateTopic("");
+          }}
+        >
+          All topics
+        </option>
+        {topics.map((topic) => {
+          return (
+            <option
+              value={topic.slug}
+              onClick={() => {
+                navigateTopic(topic.slug);
+              }}
+            >
+              {topic.slug}
+            </option>
+          );
+        })}
+      </select>
+
+      <ul className="main_articles_list">
+        {articles.map((article) => {
+          return (
+            <li key={article.article_id}>
+              <h3>{article.title}</h3>
+              <h4>topic: {article.topic}</h4>
+              <h5>written by: {article.author}</h5>
+              <h5>posted: {article.created_at}</h5>
+              <p>{article.body}</p>
+              <div className="votes_and_comments">
+                <h6 className="votes_and_comments_child1">
+                  votes: {article.votes}
+                </h6>{" "}
+                <h6 className="votes_and_comments_child2">
+                  comments: {article.comment_count}
+                </h6>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
