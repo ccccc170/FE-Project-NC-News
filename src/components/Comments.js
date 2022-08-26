c64a0a80cfa33bcbf56a000ff31f5cba056754b9
ccccc170 <ccccc170@protonmail.com>
2022-08-26 11:52:04 +0100

    this commit includes completed ticket 9, able to post a comment, prompted to sign in to be able to post a comment, comment posts optimistically and has error handling set up to remove optimistically posted comment and display message to user if unable to post

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,13 +5,21 @@ import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
 
 const Comments = (article_id) => {
-  const { user, setUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
   const [comments, setComments] = useState([]);
   const [commentText, setCommentText] = useState("");
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     getCommentsByArticleId(article_id).then((fetchedComments) => {
-      setComments(fetchedComments);
+      const sortedComments = fetchedComments.sort((a, b) => {
+        return a.created_at > b.created_at
+          ? -1
+          : a.created_at < b.created_at
+          ? 1
+          : 0;
+      });
+      setComments(sortedComments);
     });
   }, [article_id]);
 
@@ -33,61 +41,56 @@ const Comments = (article_id) => {
         return [newCommentData, ...currComments];
       });
       postComment(newComment, article_id).catch((err) => {
-        // do I need a .then block, reset state?
+        setIsError(true);
         setComments((currComments) => {
-          // will this work??? do i then need to update page again after???
-          return currComments.map((currCom) => {
+          return currComments.filter((currCom) => {
             return currCom.body !== commentText;
           });
         });
       });
+      setCommentText("");
     } else {
       window.alert("Please type a comment before trying to post!");
-      // what instead of window alerts?
     }
   };
 
-  return user === "Logged Out" ? (
+  return (
     <section>
       <h4>Comments:</h4>
-      <h5>
-        Please <Link to="/login">sign in</Link> to post a comment
-      </h5>
+      {user === "Logged Out" ? (
+        <h5>
+          Please <Link to="/login">sign in</Link> to post a comment
+        </h5>
+      ) : (
+        <label htmlFor="comment-box">
+          Post a comment as <Link to="/user">{user.username}</Link>:
+          <form id="comment-box" className="comment-form">
+            <textarea
+              className="comment-input"
+              maxLength="500"
+              value={commentText}
+              onChange={(event) => {
+                handleCommentChange(event);
+              }}
+            />
+            <button
+              className="comment-submit"
+              type="sumbit"
+              onClick={(event) => handleSubmitComment(event)}
+            >
+              Post
+            </button>
+            {isError ? (
+              <h5>
+                Unable to post comment, please refresh the page and try again!
+              </h5>
+            ) : null}
+          </form>
+        </label>
+      )}
       {comments.map((comment) => {
         return (
-          <div className="comment" key={comment.comment_id}>
-            <h5 className="comment-author">{comment.author}</h5>
-            <h6>Posted: {comment.created_at}</h6>
-            <p>{comment.body}</p>
-            <h6>votes: {comment.votes}</h6>
-          </div>
-        );
-      })}
-    </section>
-  ) : (
-    <section>
-      <h4>Comments:</h4>
-      <label htmlFor="comment-box">
-        Post a comment as <Link to="/user">{user.username}</Link>:
-        <form id="comment-box" className="comment-form">
-          <textarea
-            className="comment-input"
-            onChange={(event) => {
-              handleCommentChange(event);
-            }}
-          />
-          <button
-            className="comment-submit"
-            type="sumbit"
-            onClick={(event) => handleSubmitComment(event)}
-          >
-            Post
-          </button>
-        </form>
-      </label>
-      {comments.map((comment) => {
-        return (
-          <div className="comment" key={comment.comment_id}>
+          <div className="comment" key={comment.created_at}>
             <h5 className="comment-author">{comment.author}</h5>
             <h6>Posted: {comment.created_at}</h6>
             <p>{comment.body}</p>
